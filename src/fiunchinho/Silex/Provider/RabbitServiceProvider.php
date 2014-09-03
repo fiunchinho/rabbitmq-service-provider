5ac626003f14dbb79f004c5515850fe1e928c1bc
fiunchinho <github@armesto.net>
2014-09-04 00:03:29 +0200

    Services are lazyly loaded

--- src/fiunchinho/Silex/Provider/RabbitServiceProvider.php
+++ src/fiunchinho/Silex/Provider/RabbitServiceProvider.php
@@ -9,6 +9,8 @@ use OldSound\RabbitMqBundle\RabbitMq\Producer;
 use OldSound\RabbitMqBundle\RabbitMq\Consumer;
 use OldSound\RabbitMqBundle\RabbitMq\AnonConsumer;
 use OldSound\RabbitMqBundle\RabbitMq\MultipleConsumer;
+use OldSound\RabbitMqBundle\RabbitMq\RpcClient;
+use OldSound\RabbitMqBundle\RabbitMq\RpcServer;
 use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Connection\AMQPLazyConnection;
 
@@ -38,7 +40,7 @@ class RabbitServiceProvider implements ServiceProviderInterface
      * @param  array     $connections Connections defined in the config file.
      * @return string                 The connection name that will be used
      */
-    private function getConnectionName($options, $connections)
+    private function getConnection($app, $options, $connections)
     {
     	$connection_name = @$options['connection']?: self::DEFAULT_CONNECTION;
 
@@ -46,182 +48,205 @@ class RabbitServiceProvider implements ServiceProviderInterface
 			throw new \InvalidArgumentException('Configuration for connection [' . $connection_name . '] not found');
 		}
 
-		return 'rabbit.connection.' . $connection_name;
+		return $app['rabbit.connection'][$connection_name];
     }
 
     private function loadConnections($app)
     {
-    	if (!isset($app['rabbit.connections'])) {
-    		throw new \InvalidArgumentException('You need to specify at least a connection in your configuration.');
-    	}
+    	$app['rabbit.connection'] = $app->share(function ($app) {
+    		if (!isset($app['rabbit.connections'])) {
+	    		throw new \InvalidArgumentException('You need to specify at least a connection in your configuration.');
+	    	}
 
-    	foreach ($app['rabbit.connections'] as $name => $options) {
-			$connection = new AMQPLazyConnection(
-	        	$app['rabbit.connections'][$name]['host'],
-	        	$app['rabbit.connections'][$name]['port'],
-	        	$app['rabbit.connections'][$name]['user'],
-	        	$app['rabbit.connections'][$name]['password'],
-	        	$app['rabbit.connections'][$name]['vhost']
-	        );
+    		$connections = [];
+    		foreach ($app['rabbit.connections'] as $name => $options) {
+				$connection = new AMQPLazyConnection(
+		        	$app['rabbit.connections'][$name]['host'],
+		        	$app['rabbit.connections'][$name]['port'],
+		        	$app['rabbit.connections'][$name]['user'],
+		        	$app['rabbit.connections'][$name]['password'],
+		        	$app['rabbit.connections'][$name]['vhost']
+		        );
 
-			$app['rabbit.connection.' . $name] = $app->share(function ($app) use ($connection) {
-				return $connection;
-			});
-		}
+				$connections[$name] = $connection;
+			}
+
+			return $connections;
+    	});
     }
 
     private function loadProducers($app)
     {
-    	if (!isset($app['rabbit.producers'])) {
-    		return;
-    	}
+    	$app['rabbit.producer'] = $app->share(function ($app) {
+	    	if (!isset($app['rabbit.producers'])) {
+	    		return;
+	    	}
 
-    	foreach ($app['rabbit.producers'] as $name => $options) {
-			$connection_name = $this->getConnectionName($options, $app['rabbit.connections']);
+    		$producers = [];
+			foreach ($app['rabbit.producers'] as $name => $options) {
+				$connection = $this->getConnection($app, $options, $app['rabbit.connections']);
 
-            $producer = new Producer($app[$connection_name]);
-			$producer->setExchangeOptions($options['exchange_options']);
+	            $producer = new Producer($connection);
+				$producer->setExchangeOptions($options['exchange_options']);
 
-			if ((array_key_exists('auto_setup_fabric', $options)) && (!$options['auto_setup_fabric'])) {
-				$producer->disableAutoSetupFabric();
+				if ((array_key_exists('auto_setup_fabric', $options)) && (!$options['auto_setup_fabric'])) {
+					$producer->disableAutoSetupFabric();
+				}
+
+				$producers[$name] = $producer;
 			}
 
-			$app['rabbit.producer.' . $name] = $app->share(function ($app) use ($producer) {
-				return $producer;
-			});
-		}
+			return $producers;
+		});
     }
 
     private function loadConsumers($app)
     {
-    	if (!isset($app['rabbit.consumers'])) {
-    		return;
-    	}
+    	$app['rabbit.consumer'] = $app->share(function ($app) {
+	    	if (!isset($app['rabbit.consumers'])) {
+	    		return;
+	    	}
 
-    	foreach ($app['rabbit.consumers'] as $name => $options) {
-			$connection_name = $this->getConnectionName($options, $app['rabbit.connections']);
-            $consumer = new Consumer($app[$connection_name]);
-			$consumer->setExchangeOptions($options['exchange_options']);
-			$consumer->setQueueOptions($options['queue_options']);
-			$consumer->setCallback(array($app[$options['callback']], 'execute'));
+    		$consumers = [];
+    		foreach ($app['rabbit.consumers'] as $name => $options) {
+				$connection = $this->getConnection($app, $options, $app['rabbit.connections']);
+	            $consumer = new Consumer($connection);
+				$consumer->setExchangeOptions($options['exchange_options']);
+				$consumer->setQueueOptions($options['queue_options']);
+				$consumer->setCallback(array($app[$options['callback']], 'execute'));
 
-			if (array_key_exists('qos_options', $options)) {
-				$consumer->setQosOptions(
-					$options['qos_options']['prefetch_size'],
-					$options['qos_options']['prefetch_count'],
-					$options['qos_options']['global']
-				);
+				if (array_key_exists('qos_options', $options)) {
+					$consumer->setQosOptions(
+						$options['qos_options']['prefetch_size'],
+						$options['qos_options']['prefetch_count'],
+						$options['qos_options']['global']
+					);
+				}
+
+				if (array_key_exists('qos_options', $options)) {
+					$consumer->setIdleTimeout($options['idle_timeout']);
+				}
+
+				if ((array_key_exists('auto_setup_fabric', $options)) && (!$options['auto_setup_fabric'])) {
+					$consumer->disableAutoSetupFabric();
+				}
+
+				$consumers[$name] = $consumer;
 			}
 
-			if (array_key_exists('qos_options', $options)) {
-				$consumer->setIdleTimeout($options['idle_timeout']);
-			}
-
-			if ((array_key_exists('auto_setup_fabric', $options)) && (!$options['auto_setup_fabric'])) {
-				$consumer->disableAutoSetupFabric();
-			}
-
-			$app['rabbit.consumer.' . $name] = $app->share(function ($app) use ($consumer) {
-				return $consumer;
-			});
-		}
+			return $consumers;
+    	});
 	}
 
 	private function loadAnonymousConsumers($app)
 	{
-		if (!isset($app['rabbit.anon_consumers'])) {
-    		return;
-    	}
+    	$app['rabbit.anonymous_consumer'] = $app->share(function ($app) {
+    		if (!isset($app['rabbit.anon_consumers'])) {
+	    		return;
+	    	}
 
-		foreach ($app['rabbit.anon_consumers'] as $name => $options) {
-			$connection_name = $this->getConnectionName($options, $app['rabbit.connections']);
-            $consumer = new AnonConsumer($app[$connection_name]);
-			$consumer->setExchangeOptions($options['exchange_options']);
-			$consumer->setCallback(array($options['callback'], 'execute'));
+    		$consumers = [];
+    		foreach ($app['rabbit.anon_consumers'] as $name => $options) {
+				$connection = $this->getConnection($app, $options, $app['rabbit.connections']);
+	            $consumer = new AnonConsumer($connection);
+				$consumer->setExchangeOptions($options['exchange_options']);
+				$consumer->setCallback(array($options['callback'], 'execute'));
 
-			$app['rabbit.anonymous.' . $name] = $app->share(function ($app) use ($consumer) {
-				return $consumer;
-			});
-		}
+				$consumers[$name] = $consumer;
+			}
+
+			return $consumers;
+    	});
 	}
 
 	private function loadMultipleConsumers($app)
 	{
-		if (!isset($app['rabbit.multiple_consumers'])) {
-    		return;
-    	}
+    	$app['rabbit.multiple_consumer'] = $app->share(function ($app) {
+    		if (!isset($app['rabbit.multiple_consumers'])) {
+	    		return;
+	    	}
 
-    	foreach ($app['rabbit.multiple_consumers'] as $name => $options) {
-			$connection_name = $this->getConnectionName($options, $app['rabbit.connections']);
-            $consumer = new MultipleConsumer($app[$connection_name]);
-			$consumer->setExchangeOptions($options['exchange_options']);
-			$consumer->setQueues($options['queues']);
+    		$consumers = [];
+    		foreach ($app['rabbit.multiple_consumers'] as $name => $options) {
+				$connection = $this->getConnection($app, $options, $app['rabbit.connections']);
+	            $consumer = new MultipleConsumer($connection);
+				$consumer->setExchangeOptions($options['exchange_options']);
+				$consumer->setQueues($options['queues']);
 
-			if (array_key_exists('qos_options', $options)) {
-				$consumer->setQosOptions(
-					$options['qos_options']['prefetch_size'],
-					$options['qos_options']['prefetch_count'],
-					$options['qos_options']['global']
-				);
+				if (array_key_exists('qos_options', $options)) {
+					$consumer->setQosOptions(
+						$options['qos_options']['prefetch_size'],
+						$options['qos_options']['prefetch_count'],
+						$options['qos_options']['global']
+					);
+				}
+
+				if (array_key_exists('qos_options', $options)) {
+					$consumer->setIdleTimeout($options['idle_timeout']);
+				}
+
+				if ((array_key_exists('auto_setup_fabric', $options)) && (!$options['auto_setup_fabric'])) {
+					$consumer->disableAutoSetupFabric();
+				}
+
+				$consumers[$name] = $consumer;
 			}
 
-			if (array_key_exists('qos_options', $options)) {
-				$consumer->setIdleTimeout($options['idle_timeout']);
-			}
-
-			if ((array_key_exists('auto_setup_fabric', $options)) && (!$options['auto_setup_fabric'])) {
-				$consumer->disableAutoSetupFabric();
-			}
-
-			$app['rabbit.multiple.' . $name] = $app->share(function ($app) use ($consumer) {
-				return $consumer;
-			});
-		}
+			return $consumers;
+    	});
+    	
     }
 
     private function loadRpcClients($app)
     {
-		if (!isset($app['rabbit.rpc_clients'])) {
-    		return;
-    	}
+    	$app['rabbit.rpc_client'] = $app->share(function ($app) {
+    		if (!isset($app['rabbit.rpc_clients'])) {
+	    		return;
+	    	}
 
-    	foreach ($app['rabbit.rpc_clients'] as $name => $options) {
-			$connection_name = $this->getConnectionName($options, $app['rabbit.connections']);
-            $client = new RpcClient($app[$connection_name]);
+    		$clients = [];
+    		foreach ($app['rabbit.rpc_clients'] as $name => $options) {
+				$connection = $this->getConnection($app, $options, $app['rabbit.connections']);
+	            $client = new RpcClient($connection);
 
-			if (array_key_exists('expect_serialized_response', $options)) {
-				$client->initClient($options['expect_serialized_response']);
+				if (array_key_exists('expect_serialized_response', $options)) {
+					$client->initClient($options['expect_serialized_response']);
+				}
+
+				$clients[$name] = $client;
 			}
 
-			$app['rabbit.rpc_client.' . $name] = $app->share(function ($app) use ($client) {
-				return $client;
-			});
-		}
+			return $clients;
+    	});
     }
 
     private function loadRpcServers($app)
     {
-		if (!isset($app['rabbit.rpc_servers'])) {
-    		return;
-    	}
+    	$app['rabbit.rpc_server'] = $app->share(function ($app) {
+    		if (!isset($app['rabbit.rpc_servers'])) {
+	    		return;
+	    	}
 
-    	foreach ($app['rabbit.rpc_servers'] as $name => $options) {
-			$connection_name = $this->getConnectionName($options, $app['rabbit.connections']);
-            $server = new RpcServer($app[$connection_name]);
-            $server->initServer($name);
-            $server->setCallback(array($options['callback'], 'execute'));
+    		$servers = [];
+    		foreach ($app['rabbit.rpc_servers'] as $name => $options) {
+				$connection = $this->getConnection($app, $options, $app['rabbit.connections']);
+	            $server = new RpcServer($connection);
+	            $server->initServer($name);
+	            $server->setCallback(array($options['callback'], 'execute'));
 
-			if (array_key_exists('qos_options', $options)) {
-				$server->setQosOptions(
-					$options['qos_options']['prefetch_size'],
-					$options['qos_options']['prefetch_count'],
-					$options['qos_options']['global']
-				);
+				if (array_key_exists('qos_options', $options)) {
+					$server->setQosOptions(
+						$options['qos_options']['prefetch_size'],
+						$options['qos_options']['prefetch_count'],
+						$options['qos_options']['global']
+					);
+				}
+
+				$servers[$name] = $server;
 			}
 
-			$app['rabbit.rpc_client.' . $name] = $app->share(function ($app) use ($server) {
-				return $server;
-			});
-		}
+			return $servers;
+    	});
+    	
     }
 }
---
+++ tests/unit/RabbitServiceProviderTest.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace fiunchinho\Silex\Provider;
+
+use Silex\Application;
+
+class RabbitServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+	public function testConnectionsAreRegistered()
+	{
+		$app = new Application();
+
+        $app->register(new RabbitServiceProvider(), [
+            'rabbit.connections' => [
+            	'default' => [
+            		'host' => 'localhost',
+            		'port' => 5672,
+            		'user' => 'guest',
+            		'password' => 'guest',
+            		'vhost' => '/'
+            	],
+            	'another' => [
+            		'host' => 'localhost',
+            		'port' => 5672,
+            		'user' => 'guest',
+            		'password' => 'guest',
+            		'vhost' => '/'
+            	]
+            ]
+        ]);
+
+        $this->assertInstanceOf("PhpAmqpLib\Connection\AMQPConnection", $app['rabbit.connection']['default']);
+        $this->assertInstanceOf("PhpAmqpLib\Connection\AMQPConnection", $app['rabbit.connection']['another']);
+	}
+
+	public function testProducersAreRegistered()
+	{
+		$app = new Application();
+
+        $app->register(new RabbitServiceProvider(), [
+            'rabbit.connections' => [
+            	'default' => [
+            		'host' => 'localhost',
+            		'port' => 5672,
+            		'user' => 'guest',
+            		'password' => 'guest',
+            		'vhost' => '/'
+            	]
+            ],
+            'rabbit.producers' => [
+            	'a_producer' => [
+            		'connection' 		=> 'default',
+					'exchange_options'	=> ['name' => 'a_exchange', 'type' => 'topic']
+            	],
+            	'second_producer' => [
+            		'connection' 		=> 'default',
+					'exchange_options'	=> ['name' => 'a_exchange', 'type' => 'topic']
+            	],
+            ]
+        ]);
+
+        $this->assertInstanceOf("OldSound\RabbitMqBundle\RabbitMq\Producer", $app['rabbit.producer']['a_producer']);
+        $this->assertInstanceOf("OldSound\RabbitMqBundle\RabbitMq\Producer", $app['rabbit.producer']['second_producer']);
+	}
+
+	public function testConsumersAreRegistered()
+	{
+		$app = new Application();
+
+        $app->register(new RabbitServiceProvider(), [
+            'rabbit.connections' => [
+            	'default' => [
+            		'host' => 'localhost',
+            		'port' => 5672,
+            		'user' => 'guest',
+            		'password' => 'guest',
+            		'vhost' => '/'
+            	],
+            	'another' => [
+            		'host' => 'localhost',
+            		'port' => 5672,
+            		'user' => 'guest',
+            		'password' => 'guest',
+            		'vhost' => '/'
+            	]
+            ],
+            'rabbit.consumers' => [
+            	'a_consumer' => [
+            		'connection' 		=> 'default',
+					'exchange_options' 	=> ['name' => 'a_exchange','type' => 'topic'],
+					'queue_options' 	=> ['name' => 'a_queue', 'routing_keys' => ['foo.#']],
+					'callback' 			=> 'debug'
+            	],
+            	'second_consumer' => [
+            		'connection' 		=> 'another',
+					'exchange_options' 	=> ['name' => 'a_exchange','type' => 'topic'],
+					'queue_options' 	=> ['name' => 'a_queue', 'routing_keys' => ['#.foo.#']],
+					'callback' 			=> 'debug'
+            	],
+            ]
+        ]);
+
+        $this->assertInstanceOf("OldSound\RabbitMqBundle\RabbitMq\Consumer", $app['rabbit.consumer']['a_consumer']);
+        $this->assertInstanceOf("OldSound\RabbitMqBundle\RabbitMq\Consumer", $app['rabbit.consumer']['second_consumer']);
+	}
+
+	public function testAnonymousConsumersAreRegistered()
+	{
+		$app = new Application();
+
+        $app->register(new RabbitServiceProvider(), [
+            'rabbit.connections' => [
+            	'default' => [
+            		'host' => 'localhost',
+            		'port' => 5672,
+            		'user' => 'guest',
+            		'password' => 'guest',
+            		'vhost' => '/'
+            	],
+            	'another' => [
+            		'host' => 'localhost',
+            		'port' => 5672,
+            		'user' => 'guest',
+            		'password' => 'guest',
+            		'vhost' => '/'
+            	]
+            ],
+            'rabbit.anon_consumers' => [
+            	'anoymous' => [
+            		'connection' 		=> 'another',
+					'exchange_options' 	=> ['name' => 'exchange_name','type' => 'topic'],
+					'callback' 			=> 'debug'
+            	]
+            ]
+        ]);
+
+        $this->assertInstanceOf("OldSound\RabbitMqBundle\RabbitMq\Consumer", $app['rabbit.anonymous_consumer']['anoymous']);
+	}
+
+	public function testMultiplesConsumersAreRegistered()
+	{
+		$app = new Application();
+
+        $app->register(new RabbitServiceProvider(), [
+            'rabbit.connections' => [
+            	'default' => [
+            		'host' => 'localhost',
+            		'port' => 5672,
+            		'user' => 'guest',
+            		'password' => 'guest',
+            		'vhost' => '/'
+            	],
+            	'another' => [
+            		'host' => 'localhost',
+            		'port' => 5672,
+            		'user' => 'guest',
+            		'password' => 'guest',
+            		'vhost' => '/'
+            	]
+            ],
+            'rabbit.multiple_consumers' => [
+            	'multiple' => [
+            		'connection' 		=> 'default',
+					'exchange_options' 	=> ['name' => 'exchange_name','type' => 'topic'],
+					'queues'			=> [
+						'exchange_name' => ['name' => 'queue_name', 'routing_keys' => ['foo.#'], 'callback' => 'debug']
+					]
+            	]
+            ]
+        ]);
+
+        $this->assertInstanceOf("OldSound\RabbitMqBundle\RabbitMq\Consumer", $app['rabbit.multiple_consumer']['multiple']);
+	}
+
+	public function testRpcClientsAreRegistered()
+	{
+		$app = new Application();
+
+        $app->register(new RabbitServiceProvider(), [
+            'rabbit.connections' => [
+            	'another' => [
+            		'host' => 'localhost',
+            		'port' => 5672,
+            		'user' => 'guest',
+            		'password' => 'guest',
+            		'vhost' => '/'
+            	]
+            ],
+            'rabbit.rpc_clients' => [
+            	'a_client' => [
+            		'connection' 					=> 'another',
+					'expect_serialized_response' 	=> false
+            	]
+            ]
+        ]);
+
+        $this->assertInstanceOf("OldSound\RabbitMqBundle\RabbitMq\RpcClient", $app['rabbit.rpc_client']['a_client']);
+	}
+
+	public function testRpcServersAreRegistered()
+	{
+		$app = new Application();
+
+        $app->register(new RabbitServiceProvider(), [
+            'rabbit.connections' => [
+            	'another' => [
+            		'host' => 'localhost',
+            		'port' => 5672,
+            		'user' => 'guest',
+            		'password' => 'guest',
+            		'vhost' => '/'
+            	]
+            ],
+            'rabbit.rpc_servers' => [
+            	'a_server' => [
+            		'connection' 	=> 'another',
+			        'callback'	 	=> 'random_int_server',
+			        'qos_options' 	=> ['prefetch_size' => 0, 'prefetch_count' => 1, 'global' => false]
+            	]
+            ]
+        ]);
+
+        $this->assertInstanceOf("OldSound\RabbitMqBundle\RabbitMq\RpcServer", $app['rabbit.rpc_server']['a_server']);
+	}
+}
